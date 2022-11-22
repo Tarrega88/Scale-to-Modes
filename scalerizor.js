176e619513b3189078aefb96aa01976704aff101
Michael <85704079+Tarrega88@users.noreply.github.com>
2022-11-21 20:57:28 -0800

    Named scales and multiple at once

--- scalerizor.js
+++ scalerizor.js
@@ -1,44 +1,52 @@
-//Convert scales to modes (basic open 1 string version- will work on updating this later)
-
-
-
-let array = [0,2,3,6,7,8,11]
-
-function run() {
-
-    let everyFuckingMode = findModes(array)
-    printTheModes(everyFuckingMode)
+let rootScalesToPush = {
+    "major-": [0, 2, 4, 5, 7, 9, 11],
+    "minor-": [0, 2, 3, 5, 7, 8, 10],
+    "harmonic_minor-": [0, 2, 3, 5, 7, 8, 11],
+    "melodic_minor-": [0, 2, 3, 5, 7, 9, 11],
+    "harmonic_major-": [0, 2, 4, 5, 7, 8, 11],
+    "hungarian_minor-": [0, 2, 3, 6, 7, 8, 11]
 }
 
-function findModes(array) {
-    let newArray = []
-    let allModes = []
-    // push ionian
-    allModes.push(array)
+function fillModes() {
 
-    for (let q = 0; q < array.length - 1; q ++) {
-        for (let i = 0; i < array.length; i ++) {
-            let removalAmount = array[1]
-            newArray.push(array[i] - removalAmount)
+    let scales = {
+    }
+
+    for (let i = 0; i < Object.keys(rootScalesToPush).length; i++) {
+        let scaleToTweak = Object.values(rootScalesToPush)[i];
+        for (let j = 0; j < Object.values(rootScalesToPush)[i].length; j++) {
+            let romanNumeral = "";
+            switch (j) {
+                case 0:
+                    romanNumeral = "i";
+                    break;
+                case 1:
+                    romanNumeral = "ii";
+                    break;
+                case 2:
+                    romanNumeral = "iii";
+                    break;
+                case 3:
+                    romanNumeral = "iv";
+                    break;
+                case 4:
+                    romanNumeral = "v";
+                    break;
+                case 5:
+                    romanNumeral = "vi";
+                    break;
+                case 6:
+                    romanNumeral = "vii";
+                    break;
+            }
+            scales[Object.keys(rootScalesToPush)[i] + romanNumeral] = scaleToTweak
+            let removalAmount = scaleToTweak[1];
+            scaleToTweak = scaleToTweak.map(e => e - removalAmount);
+            scaleToTweak[0] = scaleToTweak[0] += 12;
+            scaleToTweak.sort((a, b) => a - b);
         }
-
-        newArray[0] = newArray[0] + 12
-        newArray = newArray.sort((a,b)=> a - b)
-
-        //push modes into array to return
-        allModes.push(newArray)
-        array = newArray
-        newArray = []
     }
-    return allModes
+    console.log(scales);
+    return scales;
 }
-
-function printTheModes(array) {
-    for(let i = 0; i< array.length; i++) {
-        console.log(`MODE ${i+1}`)
-        console.log(array[i])
-    }
-}
-
-run()
-
+fillModes();
